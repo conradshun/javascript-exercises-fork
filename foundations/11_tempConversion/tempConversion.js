d50a5f8a5907af2cc1ba8d36da7aed29967e6c0a
conrad <conradmativo25@gmail.com>
2026-08-24 19:03:14 +0800

    tempConversion so easy

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+
+let convertedTemp = (tempFahrenheit-32)/1.8;
+let result = Math.round(convertedTemp*10)/10;
+return result;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+
+  let convertedTemp = (tempCelsius*1.8)+32;
+  let result = Math.round(convertedTemp*10)/10;
+
+  return result;
 };
 
 // Do not edit below this line
