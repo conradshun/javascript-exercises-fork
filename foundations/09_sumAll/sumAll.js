020b4c96c86a5b76b9d4cfd8d1a609af4f559c76
conrad <conradmativo25@gmail.com>
2026-08-24 18:52:37 +0800

    leapYears and sumAll functions

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,11 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+let sum = 0;
+if(start >=0 && end >= 0){
+    for(let i = start; i <= end; i++){
+        return sum = sum + i;
+    }
+}
 
 };
 
--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+let divisorby4 = 4;
+let divisorby100 = 100;
+
+if(year % divisorby100 == 0){
+    return false;
+}else if(year % divisorby4 == 0){
+    return true;
+}
 
 };
 
